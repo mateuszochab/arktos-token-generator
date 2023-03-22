3905375927243c66d6a7c5096e94e302fc3615a7
mateuszochab <ochab.mat@gmail.com>
2023-03-22 19:36:37 +0100

    Initial commit

---
+++ src/main/java/com/analitiker/arktos/arktostokengenerator/ArktosTokenGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.analitiker.arktos.arktostokengenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArktosTokenGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArktosTokenGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analitiker/arktos/arktostokengenerator/ArktosTokenGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analitiker.arktos.arktostokengenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArktosTokenGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
